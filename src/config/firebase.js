4de11edcf9d63c345455e98375d1b18e40f56b4d
Stuart Long <chayboon@gmail.com>
2021-05-26 03:08:33 +0800

    Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// Firebase Config
+export const config = {
+    apiKey: "AIzaSyA-bROuQeYtw5ZtdGJVGyPtRmiemm7466I",
+    authDomain: "chatella-1e8ab.firebaseapp.com",
+    projectId: "chatella-1e8ab",
+    storageBucket: "chatella-1e8ab.appspot.com",
+    messagingSenderId: "998337225486",
+    appId: "1:998337225486:web:511c2d7ff5d09db7802d5f",
+    measurementId: "G-L6JY0BD56M"
+};
