e3ca0e6dbc046d8a143463b3a869644b2cbc0933
Stuart Long <chayboon@gmail.com>
2021-06-26 00:52:31 +0800

    Fixed CSS bug

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -104,7 +104,7 @@ function ChatPage(props) {
                     var userid = user.googleId
                     usersList += '<li id="' + user.googleId + '" class="' + activeClass + '" ><a href="javascript:void(0)">' + user.name + '</a><label class="chatNotificationCount"></label></li>';
                     $(document).ready(function () {
-                        document.getElementById(userid).addEventListener("click", () => selectUserChatBox(this, user.id, user.name, user.googleId), false)
+                        document.getElementById(userid).addEventListener("click", () => selectUserChatBox(document.getElementById(userid), user.id, user.name, user.googleId), false)
                     })
                 }
             });
@@ -194,7 +194,7 @@ function ChatPage(props) {
     }
 
     const updateChat = (message) => {
-        var origin = message.origin;
+        var origin = message.origin; // socket that sent the message
         var destination = message.destination;
         if (message.senderGoogleId === myUser.googleId && message.sender.includes(message.origin)) {
             // playNewMessageAudio();
@@ -204,6 +204,7 @@ function ChatPage(props) {
             // playNewMessageNotificationAudio();
             updateChatNotificationCount(message.senderGoogleId);
         }
+        // $('#messages').append('<li class="chatMessageRight">' + message.text + '</li>');
         if (allChatMessages[message.receiverGoogleId] !== undefined) {
             allChatMessages[message.receiverGoogleId].push(message);
         } else {
