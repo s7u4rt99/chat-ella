394963a71dbbe0d56f751d6f0871daabb138be0b
Stuart Long <chayboon@gmail.com>
2021-07-12 01:06:16 +0800

    Made Logout button appear below avatar

--- src/components/Header/AppShell.js
+++ src/components/Header/AppShell.js
@@ -51,59 +51,69 @@
 //         </AppBar>
 //     );
 // }
-import {IfFirebaseAuthed} from "@react-firebase/auth";
-import {AppBar, Avatar, Menu, MenuItem, Toolbar, Typography} from "@material-ui/core";
-import {useState} from "react";
+import { IfFirebaseAuthed } from "@react-firebase/auth";
+import {
+  AppBar,
+  Avatar,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { useState } from "react";
 
 function AppShell() {
-    const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleLogout = (firebase) => {
-        handleClose();
-        firebase.auth().signOut();
-    };
+  const handleLogout = (firebase) => {
+    handleClose();
+    firebase.auth().signOut();
+  };
 
-    return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <Typography variant="h6" style={{ flexGrow: 1, textAlign: "left" }}>
-                    Chatella
-                </Typography>
-                <IfFirebaseAuthed>
-                    {({ user, firebase }) => (
-                        <div>
-                            <Avatar
-                                alt={user.displayName}
-                                src={user.photoURL}
-                                aria-controls="simple-menu"
-                                aria-haspopup="true"
-                                onClick={handleClick}
-                            />
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={anchorEl}
-                                keepMounted
-                                open={Boolean(anchorEl)}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={() => handleLogout(firebase)}>
-                                    Logout
-                                </MenuItem>
-                            </Menu>
-                        </div>
-                    )}
-                </IfFirebaseAuthed>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1, textAlign: "left" }}>
+          Chatella
+        </Typography>
+        <IfFirebaseAuthed>
+          {({ user, firebase }) => (
+            <div>
+              <Avatar
+                alt={user.displayName}
+                src={user.photoURL}
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              />
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                getContentAnchorEl={null}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                transformOrigin={{ vertical: "top", horizontal: "center" }}
+              >
+                <MenuItem onClick={() => handleLogout(firebase)}>
+                  Logout
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
+        </IfFirebaseAuthed>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default AppShell;
